88b634d320e05dd14f044e960bf0056dec462733
NaimurRahmanNishat <naimurrahmun34@gmail.com>
2025-09-09 13:46:37 +0600

    compleate home page

--- src/components/home/Blogs.tsx
+++ src/components/home/Blogs.tsx
@@ -1,8 +1,28 @@
+"use client";
 import React from 'react'
+import { ShinyButton } from '../ui/ShinyButton';
+import Link from 'next/link';
+import { TBlog, useGetAllBlogsQuery } from '@/lib/features/blogs/blogApi';
+import Loading from '../shared/Loading';
+import Error from '../shared/Error';
+import BlogCard from '../blog/BlogCard';
+import { MoveRight } from 'lucide-react';
 
 const Blogs = () => {
+    // fetch blogs from backend
+    const { data, isLoading, isError } = useGetAllBlogsQuery({
+      search: "",
+      sortBy: "newest",
+      page: 1,
+      limit: 4,
+    });
+  
+    const blogs: TBlog[] = data?.blogs || [];
+  
+    if (isLoading) return <Loading />;
+    if (isError) return <Error />;
   return (
-    <section className="relative min-h-screen flex items-center justify-center overflow-hidden bg-gradient-to-br from-gray-50 via-white to-gray-100 dark:from-gray-900 dark:via-gray-950 dark:to-black">
+    <section className="relative min-h-screen flex overflow-hidden bg-gradient-to-br from-gray-50 via-white to-gray-100 dark:from-gray-900 dark:via-gray-950 dark:to-black">
       {/* Decorative Gradient Waves */}
       <div className="absolute inset-0 overflow-hidden">
         <div className="absolute top-[-20%] left-[-20%] w-[500px] h-[500px] bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400 dark:from-pink-600 dark:via-purple-700 dark:to-indigo-900 rounded-full blur-3xl opacity-30 animate-[spin_40s_linear_infinite]" />
@@ -10,13 +30,36 @@ const Blogs = () => {
       </div>
 
       {/* Content Container here */}
-      <div className="relative z-10 max-w-3xl mx-auto text-center backdrop-blur-xl bg-white/40 dark:bg-black/30 shadow-2xl rounded-2xl p-10 border border-white/30 dark:border-white/10">
-        <h2 className="text-5xl font-extrabold text-gray-900 dark:text-gray-100 drop-shadow-md">
-          📝 Blogs
-        </h2>
-        <p className="mt-6 text-lg text-gray-700 dark:text-gray-300 leading-relaxed">
-          Explore our latest blogs with a clean, premium gradient background.
-        </p>
+      <div className="max-w-screen-xl container mx-auto px-4 py-28 relative z-10">
+              {/* Heading Section */}
+        <div className="flex flex-col gap-4 md:gap-6 items-center justify-center">
+          <h3 className="px-4 py-2 w-fit rounded-full bg-gradient-to-r from-pink-400 via-purple-500 to-indigo-500 text-white font-medium font-quin">
+            My Blogs
+          </h3>
+          <h2 className="text-3xl md:text-4xl font-secondary font-bold text-purple-600 dark:text-purple-400">
+            Latest Blog Posts
+          </h2>
+          <p className="text-sm md:text-md text-gray-600 dark:text-gray-300 font-quin -mt-2 text-center">
+            Read my latest blog posts and stay up to date with my latest <br /> projects.
+          </p>
+        </div>
+      {/* content here go here */}
+              {/* Projects Grid */}
+        <div className="mt-10 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+          {blogs.map((blog) => (
+            <BlogCard key={blog._id} blog={blog} />
+          ))}
+        </div>
+        <div className="flex items-center justify-center mt-20">
+          <Link
+            href="/blogs"
+            className="flex items-center justify-center cursor-pointer gap-2 text-pink-500 font-bold hover:text-pink-600 transition-colors duration-300"
+          >
+            <ShinyButton className="px-8 py-2 rounded-md hover:bg-slate-800 group">
+              View All Blogs <span className="ml-2 group-hover:-rotate-20 transition-all duration-500"><MoveRight /></span>
+            </ShinyButton>
+          </Link>
+        </div>
       </div>
     </section>
   )
--- src/components/home/Project.tsx
+++ src/components/home/Project.tsx
@@ -1,11 +1,68 @@
+"use client";
 import React from "react";
+import ProjectCard from "../project/ProjectCard";
+import {
+  TProject,
+  useGetAllProjectsQuery,
+} from "@/lib/features/projects/projectApi";
+import Loading from "../shared/Loading";
+import Error from "../shared/Error";
+import Link from "next/link";
+import { ShinyButton } from "../ui/ShinyButton";
+import { MoveRight } from "lucide-react";
 
 const Project = () => {
+  // API call for limtted projects
+  const { data, isLoading, isError } = useGetAllProjectsQuery({
+    category: "Fullstack Development",
+    sortBy: "newest",
+    page: 1,
+    limit: 3,
+  });
+
+  // Backend data 
+  const projects: TProject[] = data?.data?.projects || [];
+
+  if (isLoading) return <Loading />;
+  if (isError) return <Error />;
+
   return (
-    <div className="relative min-h-screen flex items-center justify-center bg-white dark:bg-gray-950 overflow-hidden">
+    <div className="relative min-h-screen flex items-start justify-start bg-white dark:bg-gray-950 overflow-hidden">
       {/* Animated Grid */}
       <div className="absolute inset-0 opacity-20 bg-[linear-gradient(to_right,rgba(0,0,0,0.1)_1px,transparent_1px),linear-gradient(to_bottom,rgba(0,0,0,0.1)_1px,transparent_1px)] dark:bg-[linear-gradient(to_right,rgba(255,255,255,0.1)_1px,transparent_1px),linear-gradient(to_bottom,rgba(255,255,255,0.1)_1px,transparent_1px)] bg-[size:40px_40px] animate-pulse" />
-      Project
+
+      {/* Project Section */}
+      <div className="max-w-screen-xl container mx-auto px-4 py-28 relative z-10">
+        {/* Heading Section */}
+        <div className="flex flex-col gap-4 md:gap-6 items-center justify-center">
+          <h3 className="px-4 py-2 w-fit rounded-full bg-gradient-to-r from-pink-400 via-purple-500 to-indigo-500 text-white font-medium font-quin">
+            My Projects
+          </h3>
+          <h2 className="text-3xl md:text-4xl font-secondary font-bold text-purple-600 dark:text-purple-400">
+            Featured Projects
+          </h2>
+          <p className="text-lg md:text-xl text-gray-600 dark:text-gray-300 font-quin -mt-2 text-center">
+            Explore my highlighted projects showcasing my latest work.
+          </p>
+        </div>
+
+        {/* Projects Grid */}
+        <div className="mt-10 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+          {projects.map((project) => (
+            <ProjectCard key={project._id} project={project} />
+          ))}
+        </div>
+        <div className="flex items-center justify-center mt-20">
+          <Link
+            href="/projects"
+            className="flex items-center justify-center cursor-pointer gap-2 text-pink-500 font-bold hover:text-pink-600 transition-colors duration-300"
+          >
+            <ShinyButton className="px-8 py-2 rounded-md hover:bg-slate-800 group">
+              View All Projects <span className="ml-2 group-hover:-rotate-20 transition-all duration-500"><MoveRight /></span>
+            </ShinyButton>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
